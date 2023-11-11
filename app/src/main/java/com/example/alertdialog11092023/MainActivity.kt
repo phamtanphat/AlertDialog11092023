bb4c4ab3f3fb74793d0172ecd2d872d05fced98e
phamtanphat <phatdroid94@gmail.com>
2023-11-11 21:39:06 +0700

    single choice item

--- app/src/main/java/com/example/alertdialog11092023/MainActivity.kt
+++ app/src/main/java/com/example/alertdialog11092023/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.alertdialog11092023
 
-import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.core.view.isGone
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,42 +18,68 @@ class MainActivity : AppCompatActivity() {
         btnOpenCustomDialog = findViewById(R.id.button_open_custom_dialog)
 
         btnOpenAlertDialog.setOnClickListener {
-            AlertDialog.Builder(this@MainActivity).apply {
-                setTitle("Thông báo")
-                setMessage("Có phiên bản mới. Bạn cần cập nhật ứng dụng?")
-                setIcon(android.R.drawable.ic_menu_upload_you_tube)
-                setCancelable(false)
-
-                // Button Positive
-                setPositiveButton("Đồng ý") { _, _ ->
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Đồng ý",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-
-                // Button Negative
-                setNegativeButton("Thoát app") { _, _ ->
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Thoát",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-
-                // Button Neutral
-                setNeutralButton("Help") { _, _ ->
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Help",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-                show()
-            }
+            openAlertDialogSingleChoiceItems()
         }
+    }
 
+    private fun openAlertDialogSingleChoiceItems() {
+        var tmpIndexItemColor = -1
+        AlertDialog.Builder(this@MainActivity).apply {
+            setTitle("Pick color")
+            setCancelable(false)
 
+            val listItemColors: Array<String> = arrayOf("Red", "Green", "Blue", "Orange")
+            setSingleChoiceItems(listItemColors, -1) { dialogInterface, index ->
+                tmpIndexItemColor = index
+            }
+
+            // Button Positive
+            setPositiveButton("Ok") { _, _ ->
+                val color = listItemColors.getOrNull(tmpIndexItemColor) ?: ""
+                Toast.makeText(this@MainActivity, color, Toast.LENGTH_SHORT).show()
+            }
+
+            // Button Negative
+            setNegativeButton("Cancel") { _, _ -> }
+
+            show()
+        }
+    }
+
+    private fun openAlertDialogBasic() {
+        AlertDialog.Builder(this@MainActivity).apply {
+            setTitle("Thông báo")
+            setMessage("Có phiên bản mới. Bạn cần cập nhật ứng dụng?")
+            setIcon(android.R.drawable.ic_menu_upload_you_tube)
+            setCancelable(false)
+
+            // Button Positive
+            setPositiveButton("Đồng ý") { _, _ ->
+                Toast.makeText(
+                    this@MainActivity,
+                    "Đồng ý",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            // Button Negative
+            setNegativeButton("Thoát app") { _, _ ->
+                Toast.makeText(
+                    this@MainActivity,
+                    "Thoát",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            // Button Neutral
+            setNeutralButton("Help") { _, _ ->
+                Toast.makeText(
+                    this@MainActivity,
+                    "Help",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            show()
+        }
     }
 }
