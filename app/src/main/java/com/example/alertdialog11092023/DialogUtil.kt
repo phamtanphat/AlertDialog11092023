5eda869e625d8943f65d48912ed366026f6ed3ba
phamtanphat <phatdroid94@gmail.com>
2023-11-16 21:04:33 +0700

    callback function cho function de truyen du lieu

--- app/src/main/java/com/example/alertdialog11092023/DialogUtil.kt
+++ app/src/main/java/com/example/alertdialog11092023/DialogUtil.kt
@@ -2,13 +2,38 @@ package com.example.alertdialog11092023
 
 import android.app.Dialog
 import android.content.Context
+import android.view.Gravity
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import androidx.constraintlayout.widget.Constraints.LayoutParams
 
 class DialogUtil {
     companion object {
-        fun showLoginDialog(context: Context) {
-            val dialog = Dialog(context)
-            dialog.setContentView(R.layout.layout_login_dialog)
-            dialog.show()
+        fun showLoginDialog(context: Context, onListenLoginSuccess: ((String, String) -> Unit)? = null) {
+            Dialog(context).apply {
+                setContentView(R.layout.layout_login_dialog)
+                // Set window
+                window?.setLayout(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+                window?.setGravity(Gravity.CENTER)
+
+                // FindViewById
+                val edtAccount = findViewById<EditText>(R.id.edit_text_account)
+                val edtPassword = findViewById<EditText>(R.id.edit_text_password)
+                val btnLogin = findViewById<AppCompatButton>(R.id.button_login)
+
+                btnLogin.setOnClickListener {
+                    val accountStr: String = edtAccount.text.toString()
+                    val passwordStr: String = edtPassword.text.toString()
+
+                    if (accountStr == "phat" && passwordStr == "123") {
+                        onListenLoginSuccess?.invoke(accountStr, passwordStr)
+                    }
+                }
+
+                // Display dialog
+                show()
+            }
         }
     }
 }
